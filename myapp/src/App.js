1f2acdec446c0d9d5b3e1960eb1943a52635940f
dongyeon choi <ehddus980@gmail.com>
2022-05-25 10:18:00 +0900

    ver2

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
+import './static/fonts/font.css'
+import button from 'react-bootstrap/Button';
+import { Navbar, Container, NavDropdown, Nav } from 'react-bootstrap';
+import AliceCarousel from 'react-alice-carousel';
+import "react-alice-carousel/lib/alice-carousel.css";
+import image1 from './images/1.jpg';
+import image2 from './images/2.jpg'
 
 function App() {
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar bg="light" expand="lg">
+        <Container>
+          <Navbar.Brand href="#home">
+            <span>1-8반 개인 공간!</span>
+            </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#link">Link</Nav.Link>
+              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+
+    <div className='slider'>
+    <AliceCarousel autoPlay autoPlayInterval="3000">
+      <img src={image1} className="sliderimg" alt=""/>
+      <img src={image2} className="sliderimg" alt=""/>
+    </AliceCarousel>
+    </div>
+    
+    
     </div>
   );
 }
